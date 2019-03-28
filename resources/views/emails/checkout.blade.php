9f4ab4906336ebf377c8bb6b96f3853c60077990
Yossa Michel <ngambmicheal@gmail.com>
2019-03-28 23:25:25 +0100

    added email template

---
+++ resources/views/emails/checkout.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+    <html lang="en-US">
+    	<head>
+    		<meta charset="utf-8">
+    	</head>
+
+
+    	<body style="background: #eee; padding-top: 25px">
+
+    		<center><img src="https://gloriousenlargement.com/assets/images/LOGO.jpg" style="height: 100px"></center>
+
+
+
+
+    <div style="margin: 10%; padding:5%;background: white; margin-top: 3%">
+   		<h2>Invoice</h2>
+
+    	<p> Your Account : <b> XXXXX </b></p>
+    	<p> Billing date : <b> {{date('F d, Y')}} </b>
+    	</p>
+
+    	<hr> 
+
+    	<h2> Thank you for your business, </h2>
+
+    	The credit card ending in XXXX has benn successfully charged $XXX.X. A copy of this receipt is also in your <b>Billing Statements</b>
+
+    	If you have any questions, please let us know. We'll get back to you as soon as we can. 
+
+    	<br>
+    	Your friends, 
+    	<br>
+    	<a href='billing@'>billing@xxx.com</a>
+
+    	<hr> 
+
+    	<h2> Subscription </h2>
+
+    	For the upcoming year, beginning August XX, XXXX
+
+    	<br> 
+
+    	40 Plan [annual] - $XXX.XX
+
+    	20% annual discount(annual) - $XX.XX
+
+    </div>
+
+    	</body>
+    </html>
