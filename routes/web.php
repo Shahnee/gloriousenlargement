fe6b7036bdda9353d56884bd72e48d8e33dee80d
Sergey Mirakyan <sergeymirakyan@gmail.com>
2019-03-29 07:16:36 -0600

    local changes

--- routes/web.php
+++ routes/web.php
@@ -12,13 +12,16 @@
 */
 Route::middleware('page-cache')->get('/lp', 'PaymentController@front_page');
 Route::get('/lp/payment', 'PaymentController@subscription');
+Route::get('/lp/thank-you', 'PaymentController@open_thank_you_page');
 
 Route::get('/', 'PaymentController@do_die');
 
 
 Route::post('/', 'PaymentController@addSubscription');
 Route::get('/thank-you-downloadyourbook', 'PaymentController@thankyou')->name('thank-you-downloadyourbook');
+Route::get('/specialgift', 'PaymentController@special')->name('specialgift');
 Route::get('/special', 'PaymentController@special')->name('special');
+
 Route::get('/contact', 'PaymentController@contact')->name('contact');
 Route::get('/terms', 'PaymentController@terms')->name('terms');
 Route::get('/privacy', 'PaymentController@privacy')->name('privacy');
